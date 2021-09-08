c8e64dfff0e37e8cdffbb3ee721c7d5dc2597982
jgfontes <74912719+jgfontes@users.noreply.github.com>
2021-09-08 19:04:25 -0300

    Update Main.java

--- Personal-info-project/src/Entity/Main.java
+++ Personal-info-project/src/Entity/Main.java
@@ -5,12 +5,11 @@ public class Main {
 	public static void main(String[] args)
 	{
 		Personal_info personalInfoElement = new Personal_info(
-			"Júlio Gurgel Fontes",
+			"JÃºlio Gurgel Fontes",
 			"Brazilian",
 			"To have a big familly and play guitar for them"
 				);
 		
 		personalInfoElement.PrintInfo();
 	}
-	
 }
